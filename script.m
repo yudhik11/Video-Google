7e8aaf6fd95570da8278581d156e1cc71f858af3
yudhik11 <yudhik100@gmail.com>
2018-06-27 13:50:29 +0530

    final testing

--- script.m
+++ script.m
@@ -1,24 +1,27 @@
+%% Google Images
+
 clear all;
 data = '/home/yudhik/study/Video-Google/frames/';
-num_images = 10;
-num_clusters = 100;
+num_images = 100;
+num_clusters = 1000;
+tic
 [all_features img_features] = SIFT(data, num_images);
-
+toc
 [idx, clusters] = kmeans(all_features, num_clusters, 'Maxiter', 100, 'Display', 'iter');
+
 ranking = tf_idf(num_images, num_clusters, img_features, idx);
 
-img = imread('/home/yudhik/study/Video-Google/frame27.png');
+img = imread('/home/yudhik/study/Video-Google/frame90.png');
 
-predict(img, ranking, clusters);
+imgs = predict(img, ranking, clusters);
+
+for i = 1:size(imgs,2)
+    tmp = [data 'frame' sprintf('%d',imgs(i)) '.png'];
+    figure; imshow(tmp);
+end
 
 
-
-
-
-
-
-%%
-SVM
+%% matching features across images
 
 img1 = imread('/home/yudhik/study/Video-Google/frames/frame25.png');
 img2 = imread('/home/yudhik/study/Video-Google/frames/frame28.png');
@@ -26,10 +29,13 @@ img2 = imread('/home/yudhik/study/Video-Google/frames/frame28.png');
 [f2, d2] = vl_sift(single(rgb2gray(img2)));
 d = [d1'; d2'];
 [matches, scores] = vl_ubcmatch(d1, d2) ;
+
+%% plotting the features on the image
+img = imread('/home/yudhik/study/Video-Google/frame90.png');
+[f, d] = vl_sift(single(rgb2gray(img)));
 imshow(img);
-perm = randperm(size(f,2)) ;
-sel = perm(1:30) ;
-h1 = vl_plotframe(f(:,sel)) ;
-h2 = vl_plotframe(f(:,sel)) ;
+idx = [1:size(f,2)];
+h1 = vl_plotframe(f(:,idx)) ;
+h2 = vl_plotframe(f(:,idx)) ;
 set(h1,'color','k','linewidth',3) ;
 set(h2,'color','y','linewidth',2) ;
