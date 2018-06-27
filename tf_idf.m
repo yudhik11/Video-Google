23b383428296325631aae31e77f6ccaa54f1b6f2
yudhik11 <yudhik100@gmail.com>
2018-06-27 13:49:27 +0530

    ranking improved

--- tf_idf.m
+++ tf_idf.m
@@ -7,5 +7,15 @@ function ranking = tf_idf(num_images, num_clusters, img_features, idx)
             val = val+1;
             ranking(idx(cluster+j), i)= ranking(idx(cluster+j), i)+1;
         end
+    end
+    rarity = [];
+    for i=1:num_clusters
+        rarity = [rarity (sum(ranking(i, :) > mean(ranking(i, :))))];
+    end
+    for i = 1:num_clusters
+        for j = 1:num_images
+            ranking(i, j) = (ranking(i,j)/img_features(j))... %%term_frequency = num_of_features belong to that cluster
+                            * (log(num_images/rarity(i))); %% idf = Num_of_images which possess that cluster
+        end
     end 
 end
